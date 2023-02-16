bb40cb04cb30e75db9bf247005b7156c07e88490
Moyo <moyomonsuruoke@gmail.com>
2023-02-16 21:40:54 +0000

    hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include <string.h>
 
-/*this program prints the below string to standard output*/
 int main(void)
+	/*this program prints the below string to standard output*/
 {
 	char *str1 = "\"Programming is like building a multilingual puzzle";
+	
 	puts(str1);
 
 	return (0);
