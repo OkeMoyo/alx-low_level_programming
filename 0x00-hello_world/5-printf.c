00145396750d9bfd49e2a449b4b18a62037702de
Moyo <moyomonsuruoke@gmail.com>
2023-02-16 21:59:51 +0000

    hello printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - this is the main function
+ *
+ * Description
+ *
+ * Return - this value is always zero
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
