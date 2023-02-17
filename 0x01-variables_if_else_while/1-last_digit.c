f9d98f7526028e042302c845472f286160b52591
Moyo <moyomonsuruoke@gmail.com>
2023-02-17 20:55:22 +0000

    last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,7 +3,13 @@
 #include <stdio.h>
 
 /**
- * main comment section for betty style checker
+ * main - entry point of the program
+ * @n: a random number
+ * @last_digit: last digit of n
+ *
+ * Description: n is just a random number
+ * section header: just a description of the section
+ * Return: the returned value is the last digit of n
  */
 int main(void)
 {
@@ -19,8 +25,6 @@ int main(void)
 		printf("Last digit of %d is %d and is 0\n", n, last_digit);
 	else if ((last_digit < 6) && (last_digit != 0))
 		printf("Last digit of %d is %d and is less than 6 and not 0\n",n,last_digit);
-	/**
-	 * betty style comment for return function of main
-	 */
+
 	return (0);
 }
