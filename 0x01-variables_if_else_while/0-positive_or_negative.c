31306173da941f11fc65fb2b93d3e3141f8fae67
Moyo <moyomonsuruoke@gmail.com>
2023-02-17 19:57:17 +0000

    positive or negative if statement in c programming

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -10,7 +10,7 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n > 0) 
+	if (n > 0)
 	{
 		printf("%d is positive\n", n);
 	}
@@ -19,7 +19,7 @@ int main(void)
 		printf("%d is zero\n", n);
 	}
 	else
-	{	
+	{
 		printf("%d is negative\n", n);
 	}
 	/* betty style doc for function return goes here */
