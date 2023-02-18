91b3b9425eba2f2099ff97b2492e091b04e9006f
Moyo <moyomonsuruoke@gmail.com>
2023-02-18 20:36:59 +0000

    printing a particular output to stderr

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,2 +1,14 @@
-#!/bin/bash
-gcc $QFILE >
+#include <stdio.h>
+/**
+ * main - entry point of the program
+ *
+ * Return: prints a particular output
+ */
+int main(void)
+{
+	int c = "and that piece of art is useful \" - Dora Korpar, 2015-10-19";
+	putchar(c);
+	putchar('\n');
+
+	return (1);
+}
