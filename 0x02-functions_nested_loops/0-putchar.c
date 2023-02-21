374f35a2b22b55352fa2caf5847ca64958286999
Moyo <moyomonsuruoke@gmail.com>
2023-02-21 15:16:57 +0000

    updating the master to reflect my local changes

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+#include "_putchar.h"
+/**
+ * main - this program prints "_putchar"
+ *
+ * Return: it returns the string "_putchar"
+ */
+int main()
+{
+
+}
