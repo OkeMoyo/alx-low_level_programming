9777db430957f0a4eeffdbfb098193b8f4154586
Moyo <moyomonsuruoke@gmail.com>
2023-02-23 21:20:16 +0000

    printing all the single digits

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <unistd.h>
+#include <stdio.h>
+
+/**
+ * print_numbers - prints all single digits
+ * Return: all the numbers from 0 to 9
+ */
+
+void print_numbers(void)
+{
+	int n;
+
+	n = 0;
+	while (n < 10)
+	{
+		printf("%d", n);
+		n++;
+	}
+	write(1, "\n", 1);
+}
