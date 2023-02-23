8219924541f0f834e47f821d67dd3b39a10feb1e
Moyo <moyomonsuruoke@gmail.com>
2023-02-23 15:03:40 +0000

    fixing largest_number function

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -14,16 +14,27 @@ int largest_number(int a, int b, int c)
 
     if (a > b && b > c)
     {
-        largest = a;
+	    largest = a;
     }
     else if (b > a && a > c)
     {
-        largest = b;
+	    largest = b;
     }
     else if (c > a && a > b)
     {
-        largest = c;
+	    largest = c;
     }
-
+    else if (a > c && c > b)
+    {
+	    largest = a;
+    }
+    else if (b > a && a > c)
+    {
+	    largest = b;
+    }
+    else if (c > b && b > a)
+    {
+	    largest = c;
+    }
     return (largest);
 }
