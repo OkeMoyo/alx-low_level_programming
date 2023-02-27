dbeed062fd7277012523d2a5ac46fd724a2b0bd3
Moyo <moyomonsuruoke@gmail.com>
2023-02-27 16:47:16 +0000

    Task 2 - function to check the length of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strlen - prints the length of a string
+ * @s - the string whose length will be printed
+ * Return: 1
+ */
+
+int _strlen(char *s)
+{
+	int len;
+
+	len = sizeof(s);
+
+	return (len);
+}
