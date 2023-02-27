28a5d64677baa1f0b884ad6cecf5df9e87dc9578
Moyo <moyomonsuruoke@gmail.com>
2023-02-27 15:47:07 +0000

    Task 0 in pointers project

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * main - updates a variable value using a pointer
+ * Return: 0 I guess
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,7 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* Header file for pointers, arrays, and strings project */
+void reset_to_98(int *n);
+
+#endif
