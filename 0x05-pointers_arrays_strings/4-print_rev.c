f8196a3aa624884acf7832fa60b097a054aefa02
Moyo <moyomonsuruoke@gmail.com>
2023-02-27 21:52:36 +0000

    printing in reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -13,10 +13,13 @@ void print_rev(char *s)
 {
 	int i = strlen(s);
 
-	while (i >= 0)
+	while (i > 0)
 	{
-		_putchar(s[i]);
-		i--;
+		if (i != '\0')
+		{
+			_putchar(s[i]);
+			i--;
+		}
 	}
 	_putchar('\n');
 }
