4a197044b76bea1c16127ee3be1eb408821d7145
Moyo <moyomonsuruoke@gmail.com>
2023-02-28 19:18:20 +0000

    strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * *_strcpy - copies the string of a pointer to another pointer
+ * @dest: destination of the copy command
+ * @src: source of the copy
+ * Return: char
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	dest = src;
+
+	return (dest);
+}
