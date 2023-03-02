b0d0ca0d56e584fcf2229d729f372edd3e8caa7e
Moyo <moyomonsuruoke@gmail.com>
2023-03-02 10:04:15 +0000

    strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strcmp - compares two strings
+ * @s1: string 1
+ * @s2: string 2
+ * Return: result of comparison
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	signed int i;
+
+	if (s1 < s2)
+		i = -1;
+	else if (s1 == s2)
+		i = 0;
+	else
+		i = 1;
+	return (i);
+}
