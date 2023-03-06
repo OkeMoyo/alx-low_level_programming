ecd695097206cd4f6fd2a561beac17db0a647e64
Moyo <moyomonsuruoke@gmail.com>
2023-03-06 17:29:33 +0000

    print chessboard

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_chessboard - prints the chessboard
+ * @a: one argument
+ * Return: the chessboard
+ */
+int _putchar(char c);
+void print_chessboard(char (*a)[8])
+{
+	int i;
+	int j;
+
+	for (i = 0; a[i][7]; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			_putchar(a[i][j]);
+		}
+		_putchar('\n');
+	}
+}
