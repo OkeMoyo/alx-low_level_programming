fc44d587a8d3ae77014ca3e16af8f9463b9f09b3
Moyo <moyomonsuruoke@gmail.com>
2023-03-08 11:53:33 +0000

    puts recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,13 +7,11 @@
 
 void _puts_recursion(char *s)
 {
-	if (*s == '\0')
-		_putchar('\n');
-
 	_putchar(*s);
 	s++;
 
 	if (*s != '\0')
 		_puts_recursion(s);
-	_putchar('\n');
+	else
+		_putchar('\n');
 }
