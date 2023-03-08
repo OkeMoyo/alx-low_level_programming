68ab48726c5eaa3f9414ed1c582a2a0b6812a9a8
Moyo <moyomonsuruoke@gmail.com>
2023-03-08 16:07:23 +0000

    strlen recursion

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * _print_rev_recursion - prints a string in reverse
+ * @s: the string to be reverse-printed
+ */
+
+void _print_rev_recursion(char *s)
+{
+}
