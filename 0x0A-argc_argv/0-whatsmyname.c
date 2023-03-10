01478d2d968f98905165f5a11d860d29eec0d3dd
Moyo <moyomonsuruoke@gmail.com>
2023-03-10 21:38:48 +0000

    changing my name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints its name
+ * @argc: count
+ * @argv: vector
+ * Return: name
+ */
+
+int main(__attribute__((unused)) int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
