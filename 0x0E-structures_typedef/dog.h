b6d67a7bc884ef2c161bbe718045819be1c1537a
Moyo <moyomonsuruoke@gmail.com>
2023-03-20 21:10:36 +0000

    Task 0 - structures and typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,9 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
 #include <stdlib.h>
 #include <stdio.h>
 
+/**
+ * struct dog - dog details
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ */
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+#endif
