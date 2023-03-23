4893be7ef42334e58f011b3b71005f414475e395
Moyo <moyomonsuruoke@gmail.com>
2023-03-23 11:57:17 +0000

    Task 1 - Variadic functions

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -16,7 +16,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_start(my_numbers, n);
 
 	if (separator == NULL)
-		exit(EXIT_SUCCESS);
+		exit(EXIT_FAILURE);
 
 	for (i = 0; i < n; i++)
 	{
