cb5fa6cfb0007187b86c2bbd309355698cdf37b6
Moyo <moyomonsuruoke@gmail.com>
2023-03-24 20:22:32 +0000

    Task 3 - Variadic functions

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -10,11 +10,45 @@
 
 void print_all(const char * const format, ...)
 {
-	va_list(my_list);
+	char c;
+	int i;
+	double d;
+	char *s;
+	va_list (my_list);
+	const char *p = format;
 
-	format[0] = c;
-	format[1] = i;
-	format[2] = f;
-	format[3] = s;
 	va_start(my_list, format);
+
+	while (*p)
+	{
+		if (*p == 'c')
+		{
+			c = va_arg(my_list, int);
+			printf("%c", c);
+		}
+		else if (*p == 'i')
+		{
+			i = va_arg(my_list, int);
+			printf("%d", i);
+		}
+		else if (*p == 'f')
+		{
+			d = va_arg(my_list, double);
+			printf("%f", d);
+		}
+		else if (*p == 's')
+		{
+			s = va_arg(my_list, char *);
+			if (s == NULL)
+				printf("(nil)");
+			else
+				printf("%s", s);
+		}
+
+		if (*(++p))
+			printf(", ");
+	}
+
+	va_end(my_list);
+	printf("\n");
 }
