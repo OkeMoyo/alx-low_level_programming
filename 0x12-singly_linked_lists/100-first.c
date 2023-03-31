478e15d902f8a239aaac9b958575679d54f501eb
Moyo <moyomonsuruoke@gmail.com>
2023-03-31 22:44:34 +0000

    running a function before main function

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * before_main - function that executes before main
+ */
+
+void first(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
