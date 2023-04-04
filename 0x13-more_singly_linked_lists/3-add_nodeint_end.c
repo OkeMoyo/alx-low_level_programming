e93354ccfd5bc0e23714e59816acee58368390e0
Moyo <moyomonsuruoke@gmail.com>
2023-04-04 09:31:13 +0000

    adding a node at the end of the list

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -16,6 +16,11 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	listint_t *new_node;
 
 	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
 	new_node->n = n;
 	new_node->next = NULL;
 
@@ -30,6 +35,5 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 		}
 		current->next = new_node;
 	}
-	free(new_node);
 	return (&(**head));
 }
